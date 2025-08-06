f1c2869b3df7ef490a03a60a1793e68d2a282fe5
kanephan1909 <pkhang962@gmail.com>
2025-08-07 00:31:08 +0700

    implements API login and register, add access token into cookie

--- pos_backend/app.js
+++ pos_backend/app.js
@@ -1,18 +1,29 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
-const config = require("./config/config")
+const config = require("./config/config");
+const cookieParser = require("cookie-parser");
 const app = express();
 
-const PORT = config.PORT;
+const PORT = config.port;
+
 connectDB();
 
-// Root Endpoint
-app.get("/", (req, res) => {
-    res.json({message : "Hello from POS Server"});
-})
+//Middlewares
+app.use(express.json()); //Đảm bảo dòng này đứng trước route
+app.use(cookieParser())
 
-// Server
+//Routes
+app.use("/api/user", require("./routes/userRoute"));
+
+//Error Handling
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
+
 app.listen(PORT, () => {
-    console.log(`POS Server is listening on port ${PORT}`)
-})
+  console.log(`POS Server is listening on port ${PORT}`);
+});
---
+++ pos_backend/routes/userRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { register, login, getUserDate } = require("../controllers/userController");
+const { isVerifiedUser } = require("../middlewares/tokenVerification");
+const router = express.Router();
+
+// Authentication Routes
+router.post("/register", register);
+router.post("/login", login);
+
+router.route("/").get(isVerifiedUser, getUserDate);
+
+module.exports = router;
